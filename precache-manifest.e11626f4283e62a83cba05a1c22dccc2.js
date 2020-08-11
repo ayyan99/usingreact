0d1f9a857dd2c776a14bcae3488f359302e3270a
unknown <aakulbarkakoty99@gmail.com>
2020-08-11 18:10:18 +0530

    Updates

---
+++ precache-manifest.e11626f4283e62a83cba05a1c22dccc2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a70f52b5954691bc678104e4401e1b65",
+    "url": "/usingreact/index.html"
+  },
+  {
+    "revision": "59ab69453b803c3c5a96",
+    "url": "/usingreact/static/js/2.f479803d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/usingreact/static/js/2.f479803d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9512d51db6a9ee7021b0",
+    "url": "/usingreact/static/js/main.018ee12e.chunk.js"
+  },
+  {
+    "revision": "ae2b82cf08e851f32258",
+    "url": "/usingreact/static/js/runtime-main.4d6f67cb.js"
+  },
+  {
+    "revision": "dd971b1d7e472cf1d43d6873f89bf447",
+    "url": "/usingreact/static/media/logo.dd971b1d.jpg"
+  }
+]);
